c9e3c305c126708a611b6cba5f78a98bfc9875c9
fulton <gprlabs@outlook.com>
2020-05-18 07:34:13 -0700

    got other stuff working

--- tf_results.py
+++ tf_results.py
@@ -8,7 +8,12 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 import tokenization
+import transformers
+print(transformers.__version__)
+from transformers import AutoTokenizer
 import os
+import tensorflow_datasets
+
 
 # Any results you write to the current directory are saved as output.
 import numpy as np
@@ -60,21 +65,35 @@ def build_model(bert_layer, max_len=512):
     model.compile(Adam(lr=2e-6), loss='binary_crossentropy', metrics=['accuracy'])
     
     return model
+def setUp():
+    max_seq_length = 128  # Your choice here.
+    module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
+    bert_layer = hub.KerasLayer(module_url, trainable=True)
+    model = build_model(bert_layer, max_len=max_seq_length)
+    model.summary()
+    vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
+    do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
+    tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
+    data = bert_encode("hello", tokenizer, max_seq_length)
+    padded_inputs = tf.keras.preprocessing.sequence.pad_sequences(data, padding='post')
+    input_word_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
+                                    name="input_word_ids")
+    input_mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
+                                    name="input_mask")
+    segment_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
+                                        name="segment_ids")
+    pooled_output, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+
+    embedding = tf.keras.layers.Embedding(input_dim=5000, output_dim=16, mask_zero=True)
+    masked_output = embedding(padded_inputs)
+    print(masked_output._keras_mask)
+    model.call(masked_output)
 class __init__():
+    
     def __init__(self):
-        max_seq_length = 128  # Your choice here.
-        module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
-        bert_layer = hub.KerasLayer(module_url, trainable=True)
-        model = build_model(bert_layer, max_len=max_seq_length)
-        model.summary()
-        vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
-        do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
-        tokenizer = tokenization.FullTokenizer(vocab_file, do_lower_case)
-        data = bert_encode("hello", tokenizer, max_seq_length)
-        padded_inputs = tf.keras.preprocessing.sequence.pad_sequences(data, padding='post')
-        embedding = tf.keras.layers.Embedding( dtype=tf.float32, input_dim=5000, output_dim=128
-        ,input_length=None, mask_zero=True)
-        masked_output = embedding(padded_inputs)
-        model.call(masked_output)
+        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+        model = transformers.AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
     def getAll():
         print("test")
+    
