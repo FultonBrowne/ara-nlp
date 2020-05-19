98b175104dce5103714a52a19bc831695e7c72f3
fulton <gprlabs@outlook.com>
2020-05-19 16:30:18 -0700

    working

--- train.py
+++ train.py
@@ -1,2 +1,10 @@
 import torch
+import pandas as pb
 from transformers import *
+
+tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
+
+model = AutoModelWithLMHead.from_pretrained("bert-large-uncased")
+
+csvdata = pb.read_csv('file.csv', encoding='utf-8')
+ 
