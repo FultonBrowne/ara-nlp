b43a4ae00cb487e44c664251420f295eea9700fa
fulton <gprlabs@outlook.com>
2020-05-21 16:02:14 -0700

    data mask

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ def main():
         output_hidden_states = False,)
 
     df = pb.read_csv('data.csv', encoding='utf-8', header= 0, sep='\t')
-    print(df.get("sentence"))
+    mapmain = {"call": 0, "text": 1, "cal": 2, "email": 3, "weather": 4, "nav": 5, "pq": 6, "iot_off":7, "iot_on":8, "timer_resume":9,"timer_pause": 10, "timer": 11 }
 
     print('Number of training sentences: {:,}\n'.format(df.shape[0]))
 
