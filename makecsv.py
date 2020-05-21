5158de02374d654c5a43f3c8dc2bda541540cde8
fulton <gprlabs@outlook.com>
2020-05-21 14:59:55 -0700

    data stuff

---
+++ makecsv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {'sentences':  ['First value', 'Second value'],
+        'labels': ['First value', 'Second value'],
+         
+        }
+
+df = pd.DataFrame (data, columns = ['sentences','labels'])
+df.to_csv("data.csv", sep='\t', encoding='utf-8')
