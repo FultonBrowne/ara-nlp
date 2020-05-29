55a1203d91039b8b3c4b2f2eb3fbe0ec7d274d33
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-05-29 20:13:30 +0000

    working on data gen

--- datagen.py
+++ datagen.py
@@ -9,7 +9,7 @@ def main():
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     print("names")
-    path = download("https://raw.githubusercontent.com/hadley/data-baby-names/master/baby-names.csv", "./data/names.csv")
+    path = download("https://raw.githubusercontent.com/hadley/data-baby-names/master/baby-names.csv", "./data/names.csv", replace=True)
     names = pd.read_csv("./data/names.csv")
     toInput = names.name.values
     callList = []
@@ -19,4 +19,9 @@ def main():
         callList.append("call " + i)
         textList.append("text " + i)
         textList.append("message " + i)
+    navList = []
+    weatherList = []
+    download("https://developers.google.com/adwords/api/docs/appendix/geo/geotargets-2020-03-03.csv", "./data/places.csv")
+    pd.read_csv("./data/places.csv")
+    mainData = pd.read_csv("data.csv", sep="\t")
 main()
