d50741eb8b86987cbfbb7e5dc547d90bcd1de3cf
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-05-30 19:38:35 +0000

    adding fast food data

--- datagen.py
+++ datagen.py
@@ -29,17 +29,22 @@ def main():
         allList.append(toFormat("dial " + i,"call"))
         allList.append(toFormat("text " + i, "text"))
         allList.append(toFormat("message " + i, "text"))
+    placesLink="https://arafilestore.file.core.windows.net/ara-server-files/FastFoodRestaurants.csv?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2024-04-01T22:11:11Z&st=2019-12-19T15:11:11Z&spr=https&sig=lfjMHSahA6fw8enCbx0hFTE1uAVJWvPmC4m6blVSuuo%3D"
+    download(placesLink, "./data/ff.csv")
     download("https://developers.google.com/adwords/api/docs/appendix/geo/geotargets-2020-03-03.csv", "./data/places.csv")
     places = pd.read_csv("./data/places.csv").names.values
     for p in places:
         allList.append(Template("directions to " + p, "nav"))
         allList.append(Template("how far is " + p, "nav"))
         allList.append(Template("weather in " + p, "weath"))
-        allList.append(Template("what is the weather in " + p), "weath")
+        allList.append(Template("what is the weather in " + p, "weath"))
+    for f in ff.names.values:
+        allList.append(Template())
     mainData = pd.read_csv("data.csv", sep="\t")
     print(len(allList))
     for t in allList:
         print(len(mainData))
         mainData.loc[len(mainData)+1] = [len(mainData)+1, t.data, t.label]
+    df.to_csv("data.csv", sep='\t')
     print("done")
 main()
