18892cb5162200a277d08ca7d458daadaaeb24ea
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-13 13:10:50 +0000

    NLP stuff

--- __init__.py
+++ __init__.py
@@ -9,17 +9,17 @@ from flask import Flask, request
 tf_get = tf_results.__init__()
 app = Flask(__name__)
 @app.route("/v0/intent")
-def intent():
-    data = request.args.get("input")
-    print(data)
-    jsondata = jsonformat.getData(tf_get.getIntent(data))
-    return json.dumps(jsondata)
+#def intent():
+#    data = request.args.get("input")
+#   print(data)
+#   jsondata = jsonformat.getData(tf_get.getIntent(data))
+#   return json.dumps(jsondata)
 @app.route("/v0/pos")
 def pos():
     data = request.args.get("input")
     lang = request.args.get("lang")
     return json.dumps(jsonformat.getData(tf_get.getPos(data, lang)))
-@app.run("/v0/dpos")
+@app.route("/v0/dpos")
 def dpos():
     data = request.args.get("input")
     lang = request.args.get("lang")
--- tf_results.py
+++ tf_results.py
@@ -8,10 +8,10 @@ class __init__():
 
     def __init__(self):
        self.tokenizer = AutoTokenizer.from_pretrained("./model_save")
-       config = BertConfig.from_pretrained("./model_save", num_labels=13,
-            output_attentions=False, output_hidden_states=False,)
-       self.model = AutoModelForSequenceClassification.from_config(config)
-       self.model.eval()
+#       config = BertConfig.from_pretrained("./model_save", num_labels=13,
+#            output_attentions=False, output_hidden_states=False,)
+#       self.model = AutoModelForSequenceClassification.from_config(config)
+#       self.model.eval()
        self.spacymods = {"en": spacy.load("en_core_web_sm"), "de": spacy.load("de_core_news_sm")}
 
        
@@ -22,44 +22,44 @@ class __init__():
 
 
 
-    def getIntent(self, data):
-        input_ids = []
-        attention_masks = []
-        encoded_dict = self.tokenizer.encode_plus(
-                        data,                      # Sentence to encode.
-                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
-                        max_length = 64,           # Pad & truncate all sentences.
-                        pad_to_max_length = True,
-                        return_attention_mask = True,   # Construct attn. masks.
-                        return_tensors = 'pt',     # Return pytorch tensors.
-                   )
-    
-        # Add the encoded sentence to the list.    
-        input_ids.append(encoded_dict['input_ids'])
-    
-        # And its attention mask (simply differentiates padding from non-padding).
-        attention_masks.append(encoded_dict['attention_mask'])
-
-        # Convert the lists into tensors.
-        input_ids = torch.cat(input_ids, dim=0)
-        attention_masks = torch.cat(attention_masks, dim=0)
-        tokens_tensor = torch.tensor(input_ids)
-        segments_tensors = torch.tensor(attention_masks)
-
-        # Print sentence 0, now as a list of IDs.
-        print('Original: ', data)
-        print('Token IDs:', input_ids[0])
-        outputs = self.model(tokens_tensor, segments_tensors)
-        print(outputs)
-        predictions = torch.argmax(outputs[0])
-        print(predictions)
-        import wordintmap
-        ogmap = wordintmap.getData()
-        inv_map = {v: k for k, v in ogmap.items()}
-        data =predictions.tolist()
-        text = inv_map[data]
-        print(data)
-        return [{'type': 'intent', 'data': text}]
+#    def getIntent(self, data):
+#        input_ids = []
+#        attention_masks = []
+#        encoded_dict = self.tokenizer.encode_plus(
+#                        data,                      # Sentence to encode.
+#                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+#                        max_length = 64,           # Pad & truncate all sentences.
+#                        pad_to_max_length = True,
+#                        return_attention_mask = True,   # Construct attn. masks.
+#                        return_tensors = 'pt',     # Return pytorch tensors.
+#                   )
+#    
+#        # Add the encoded sentence to the list.    
+#        input_ids.append(encoded_dict['input_ids'])
+#    
+#        # And its attention mask (simply differentiates padding from non-padding).
+#        attention_masks.append(encoded_dict['attention_mask'])
+#
+#        # Convert the lists into tensors.
+#        input_ids = torch.cat(input_ids, dim=0)
+#        attention_masks = torch.cat(attention_masks, dim=0)
+#        tokens_tensor = torch.tensor(input_ids)
+#        segments_tensors = torch.tensor(attention_masks)
+#
+#        # Print sentence 0, now as a list of IDs.
+#        print('Original: ', data)
+#        print('Token IDs:', input_ids[0])
+#        outputs = self.model(tokens_tensor, segments_tensors)
+#        print(outputs)
+#        predictions = torch.argmax(outputs[0])
+#        print(predictions)
+#        import wordintmap
+#        ogmap = wordintmap.getData()
+#        inv_map = {v: k for k, v in ogmap.items()}
+#        data =predictions.tolist()
+#        text = inv_map[data]
+#        print(data)
+#        return [{'type': 'intent', 'data': text}]
     def getPos(self, data, lang):
         print(data)
         datasets = self.spacymods.get(lang)
