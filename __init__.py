913f7de183a696afc1e47df5b9013ae3a1f039c9
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-13 13:18:25 +0000

    bug fix

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import jsonformat
 import tf_results
 import os
 import json
-from flask import Flask, request
+from flask import Flask, request, Response
 
 tf_get = tf_results.__init__()
 app = Flask(__name__)
@@ -18,12 +18,12 @@ app = Flask(__name__)
 def pos():
     data = request.args.get("input")
     lang = request.args.get("lang")
-    return json.dumps(jsonformat.getData(tf_get.getPos(data, lang)))
+    return Response(json.dumps(jsonformat.getData(tf_get.getPos(data, lang))), mimetype="application/json")
 @app.route("/v0/dpos")
 def dpos():
     data = request.args.get("input")
     lang = request.args.get("lang")
-    return json.dumps(jsonformat.getData(tf_get.getDpos(data, lang)))
+    return Response(json.dumps(jsonformat.getData(tf_get.getDpos(data, lang))), mimetype="application/json")
 
 if __name__ == "__main__":
     app.run()
